be41c26fe7da248a9a9a9f8b20d1b491835029b6
daizhenyu <1449308021@qq.com>
2023-12-20 15:59:44 +0800

    rocketmq禁消费controller和pull消费者interceptor-2
    Signed-off-by: daizhenyu <1449308021@qq.com>

---
+++ sermant-plugins/sermant-mq-consume-prohibition/rocketmq-plugin/src/main/java/com/huaweicloud/sermant/mq/prohibition/rocketmq/interceptor/RocketMqPushConsumerStartInterceptor.java
@@ -0,0 +1,100 @@
+/*
+ *  Copyright (C) 2023-2023 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+package com.huaweicloud.sermant.mq.prohibition.rocketmq.interceptor;
+
+import com.huaweicloud.sermant.config.ProhibitionConfigManager;
+import com.huaweicloud.sermant.core.plugin.agent.entity.ExecuteContext;
+import com.huaweicloud.sermant.core.plugin.agent.interceptor.AbstractInterceptor;
+import com.huaweicloud.sermant.rocketmq.controller.RocketMqPushConsumerController;
+import com.huaweicloud.sermant.rocketmq.extension.RocketMqConsumerHandler;
+import com.huaweicloud.sermant.rocketmq.wrapper.DefaultMqPushConsumerWrapper;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+
+import java.util.Optional;
+
+/**
+ * RocketMq pushConsumer启动拦截器
+ *
+ * @author daizhenyu
+ * @since 2023-12-04
+ **/
+public class RocketMqPushConsumerStartInterceptor extends AbstractInterceptor {
+    private RocketMqConsumerHandler handler;
+
+    /**
+     * 无参构造方法
+     */
+    public RocketMqPushConsumerStartInterceptor() {
+    }
+
+    /**
+     * 有参构造方法
+     *
+     * @param handler 拦截点处理器
+     */
+    public RocketMqPushConsumerStartInterceptor(RocketMqConsumerHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public ExecuteContext before(ExecuteContext context) throws Exception {
+        if (handler != null) {
+            handler.doBefore(context);
+        }
+        return context;
+    }
+
+    @Override
+    public ExecuteContext after(ExecuteContext context) throws Exception {
+        Object consumerObject = context.getObject();
+        if (consumerObject != null && consumerObject instanceof DefaultMQPushConsumer) {
+            DefaultMQPushConsumer pushConsumer = (DefaultMQPushConsumer) consumerObject;
+            RocketMqPushConsumerController.cachePushConsumer(pushConsumer);
+        }
+
+        Optional<DefaultMqPushConsumerWrapper> pushConsumerWrapperOptional =
+                RocketMqPushConsumerController.getPushConsumerWrapper(consumerObject);
+        DefaultMqPushConsumerWrapper pushConsumerWrapper = null;
+        if (pushConsumerWrapperOptional.isPresent()) {
+            pushConsumerWrapper = pushConsumerWrapperOptional.get();
+            pushConsumerWrapper.setSubscribedTopics(pushConsumerWrapper.getPushConsumerImpl()
+                    .getSubscriptionInner().keySet());
+        }
+
+        if (handler != null) {
+            handler.doAfter(context);
+            return context;
+        }
+
+        // 消费者启动会根据缓存的禁消费配置对消费者执行禁消费
+        disablePushConsumption(pushConsumerWrapper);
+        return context;
+    }
+
+    @Override
+    public ExecuteContext onThrow(ExecuteContext context) {
+        return context;
+    }
+
+    private void disablePushConsumption(DefaultMqPushConsumerWrapper pushConsumerWrapper) {
+        if (pushConsumerWrapper != null) {
+            RocketMqPushConsumerController.disablePushConsumption(pushConsumerWrapper,
+                    ProhibitionConfigManager.getRocketMqProhibitionTopics());
+        }
+    }
+}
