903101ab37e864415af9cd0d7d7f3a4468a616d6
daizhenyu <1449308021@qq.com>
2024-07-17 15:35:23 +0800

    xDS Service Discovery Integration Log
    Signed-off-by: daizhenyu <1449308021@qq.com>

--- sermant-integration-tests/xds-service-test/product/spring-client/start.sh
+++ sermant-integration-tests/xds-service-test/product/spring-client/start.sh
@@ -1,1 +1,1 @@
-exec java -javaagent:/home/agent/sermant-agent.jar -jar /home/spring-client.jar
+exec java -javaagent:/home/agent/sermant-agent.jar -jar -Dsermant_log_dir=/log/sermant-logs/sermant /home/spring-client.jar > /log/sermant-logs/client.log 2>&1
