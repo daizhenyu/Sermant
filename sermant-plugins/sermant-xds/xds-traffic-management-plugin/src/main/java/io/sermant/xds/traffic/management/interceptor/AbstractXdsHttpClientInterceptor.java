a1b9b068fe2a627effb0ef612d4483d30670b391
daizhenyu <1449308021@qq.com>
2025-03-20 11:53:23 +0800

    xds plugin 0320-6
    Signed-off-by: daizhenyu <1449308021@qq.com>

--- sermant-plugins/sermant-xds/xds-traffic-management-plugin/src/main/java/io/sermant/xds/traffic/management/interceptor/AbstractXdsHttpClientInterceptor.java
+++ sermant-plugins/sermant-xds/xds-traffic-management-plugin/src/main/java/io/sermant/xds/traffic/management/interceptor/AbstractXdsHttpClientInterceptor.java
@@ -268,9 +268,7 @@ public abstract class AbstractXdsHttpClientInterceptor extends InterceptorSuppor
             FlowControlScenario scenarioInfo) {
         XdsLoadBalancer loadBalancer = XdsLoadBalancerFactory.getLoadBalancer(scenarioInfo.getServiceName(),
                 scenarioInfo.getClusterName());
-        ServiceInstance serviceInstance = loadBalancer.selectInstance(new ArrayList<>(instanceSet));
-        RetryContext.INSTANCE.updateRetriedServiceInstance(serviceInstance);
-        return serviceInstance;
+        return loadBalancer.selectInstance(new ArrayList<>(instanceSet));
     }
 
     private void removeCircuitBreakerInstance(FlowControlScenario scenarioInfo, Set<ServiceInstance> instanceSet) {
